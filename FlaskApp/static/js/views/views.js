33a3d2164bb668364182a3ddf8c1aab112bb8544
nick-singh <nicholas.chamansingh@gmail.com>
2015-03-28 01:17:01 -0400

    highlighting tweets

--- FlaskApp/static/js/views/views.js
+++ FlaskApp/static/js/views/views.js
@@ -143,20 +143,11 @@
 		},
 
 		render : function(){
-			this.model.text = this.urlify(this.model.text);
-
+			// this.model.text = this.urlify(this.model.text);
+			this.model.text = this.model.text.parseURL().parseHashtag().parseUsername();
 			$(this.el).html(this.template(this.model));			
 			
 			return this;
-		},
-
-		urlify : function(text) {
-		    var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
-		    //var urlRegex = /(https?:\/\/[^\s]+)/g;
-		    return text.replace(urlRegex, function(url,b,c) {
-		        var url2 = (c == 'www.') ?  'http://' +url : url;
-		        return '<a href="' +url2+ '" target="_blank">' + url + '</a>';
-		    }); 
 		}
 	});
 
