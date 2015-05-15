5df34d4ce936aee5d47e335b4a6620fc985c20fb
nick-singh <nicholas.chamansingh@gmail.com>
2015-05-15 14:27:57 -0400

    small fix

--- FlaskApp/static/js/controllers/controllers.js
+++ FlaskApp/static/js/controllers/controllers.js
@@ -142,8 +142,8 @@
                   }
               }
         }; 		
-
-		$scope.change = function(){			
+// 
+		// $scope.change = function(){			
 			var end = parseInt($scope.week),
 			start = end - (7 * 86400);	
 			init(start, end);
@@ -153,8 +153,8 @@
 		function init(start, end){
 			Factories.selectMenuItem('tweetsdetails');
 			// 1414668800,1427673600
-			// var hashAssoPromise = getHashtagAssociation.get(start, end, $scope.param);
-			var hashAssoPromise = getHashtagAssociation.get(1414668800,1427673600, $scope.param);
+			var hashAssoPromise = getHashtagAssociation.get(start, end, $scope.param);
+			// var hashAssoPromise = getHashtagAssociation.get(1414668800,1427673600, $scope.param);
 
 			hashAssoPromise.then(function(res){	
 				var data = res.data.hashtags,
@@ -175,8 +175,8 @@
 			});				
 
 			// 1414668800,1427673600
-			var tweetTextPromise = getTweetTextByTime.get(1414668800,1427673600, $scope.param);
-			// var tweetTextPromise = getTweetTextByTime.get(start, end, $scope.param);
+			// var tweetTextPromise = getTweetTextByTime.get(1414668800,1427673600, $scope.param);
+			var tweetTextPromise = getTweetTextByTime.get(start, end, $scope.param);
 
 			tweetTextPromise.then(function(res){	
 				var data = res.data.tweets;					
